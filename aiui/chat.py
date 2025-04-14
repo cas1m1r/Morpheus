6464e0330a781bf3012dc01b727045325ba3b249
C A S 1 M 1 R <50683460+cas1m1r@users.noreply.github.com>
2025-04-13 21:46:10 -0400

    Add files via upload

--- aiui/chat.py
+++ aiui/chat.py
@@ -1,22 +1,25 @@
-import os
-
 from flask import Flask, render_template, request, jsonify
 import requests, json, markdown,time
 from datetime import datetime
 from dotenv import load_dotenv
-app = Flask(__name__)
+from api import *
+import os
 
+# spinup flask
+app = Flask(__name__)
+# Create record of the conversation
 now = datetime.now()
 logfile = f'conversation_{now.month}-{now.day}-{now.year}-{now.hour}.md'
 
+load_dotenv()
+api_url = os.environ['URL']
+
 
 # Function to call Ollama API (example function)
-def call_ollama_api(text):
-    # Replace this with your actual API endpoint
-    api_url = "http://192.168.8.203:11434/api/chat"
+def call_ollama_api(text, api_url):
     # Set up the headers and data for the request
     headers = {'Content-Type': 'application/json',}
-    data = {"model": "gemma3:4b","messages":[{"role":"user","content":text}]}
+    data = {"model": "gemma3:4b", "messages": [{"role": "user", "content": text}]}
     # make the request
     try:
         response = requests.post(api_url, json=data, headers=headers)
@@ -31,8 +34,6 @@ def call_ollama_api(text):
 
 
 def send_ollama_image(data):
-    load_dotenv()
-    api_url = os.environ['URL']
     # Set up the headers and data for the request
     headers = {'Content-Type': 'application/json', }
     data = {"model": "gemma3:4b",
@@ -55,8 +56,9 @@ def send_ollama_image(data):
 def submit():
     tstamp1 = time.ctime()
     user_message = request.form.get('user_input')
-    bot_response = call_ollama_api(user_message)
-    msg_md = markdown.markdown(bot_response)
+    client = setup_client(api_url)
+    bot_response = ask_model(client,'gemma3:4b',user_message)
+    msg_md = markdown.markdown(bot_response['message']['content'])
     tstamp2 = time.ctime()
     reply_content = f' [{tstamp2}] **Response:**\n```html\n<html>{msg_md}\n</html>```'
     # append conversation data to logfile
@@ -81,10 +83,23 @@ def index():
     return render_template('index.html', user_message=user_message, bot_response=bot_response)
 
 
+@app.route('/models', methods=['GET'])
+def display_models():
+    client = setup_client(api_url)
+    models = list_models(client)
+    return render_template('models.html', models=models)
+
+
+# TODO: make get-model method (download new models)
+# TODO: make delete model method
+# TODO: make switch model method (one being used for chat)
+# TODO: Fix Image Upload functionality
+
+
 if __name__ == '__main__':
     # create logfile
-    with open(logfile,'w') as f:
+    with open(logfile, 'w') as f:
         f.write(f'# Log Created\n')
     f.close()
     # run application
-    app.run(debug=True)
+    app.run()
